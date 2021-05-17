8e051c6412be7f4e7f016f7584cb56c3f70b86fe
Felix Lopez <felixlop@usc.edu>
2021-05-17 15:47:51 -0700

    Assignment Done, pt. 2

--- day1.js
+++ day1.js
@@ -1,2 +1,18 @@
-#
-Question 1
+// Question 1
+
+function firstNonConsecutive(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i - 1] + 1 !== arr[i]) return arr[i];
+    }
+    return null;
+}
+
+console.log(firstNonConsecutive(0, 1, 2, 3, 4, 6, 7, 8, 9));
+console.log(firstNonConsecutive([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]));
+
+//Question 2
+
+arr == ["Max", "Baseball", "Reboot", "Goku", "Trunks", "Rodger"]
+
+let spliceUp = arr.splice(0, 2, 4);
+str.replace("even index")
